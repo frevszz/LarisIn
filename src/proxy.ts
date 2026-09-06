c9c031fa0a1750f0de5292f137afa2f0890a77fb
abcdavk <xloot.mcpedl@gmail.com>
2026-09-06 18:08:09 +0700

    fix: update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -12,13 +12,15 @@ export default clerkMiddleware(async (auth, req) => {
   if (isProtectedPage || isProtectedApi) {
     await auth.protect();
   }
+
+  
 });
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
-    '/__clerk/(.*)',
+    "/__clerk/:path*"
   ],
 };
 
