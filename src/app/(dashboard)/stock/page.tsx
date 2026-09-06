d97dbcc396b6016b8c5306ab06e9bbff0de4a18d
Farell Dio Revianzha <farelldio1@gmail.com>
2026-09-06 19:29:48 +0700

    fix:readme

--- src/app/(dashboard)/stock/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function StockPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <section className="mx-auto max-w-6xl px-4 py-6 pb-24 md:px-8 md:py-8 md:pb-8"></section>
-    </div>
-  )
-}
